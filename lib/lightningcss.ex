305fb03b0a917e38a96f23f178d938a6cd86c8f5
Pedro <pedro@pepicrft.me>
2023-09-26 15:50:27 +0200

    Create project

---
+++ lib/lightningcss.ex
@@ -0,0 +1,18 @@
+defmodule Lightningcss do
+  @moduledoc """
+  Documentation for `Lightningcss`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lightningcss.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lightningcss_test.exs
@@ -0,0 +1,8 @@
+defmodule LightningcssTest do
+  use ExUnit.Case
+  doctest Lightningcss
+
+  test "greets the world" do
+    assert Lightningcss.hello() == :world
+  end
+end
